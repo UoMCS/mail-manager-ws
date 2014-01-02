e2fc1940fefe1259c64a213fefc262c8f0026472
Paul Waring <paul.waring@manchester.ac.uk>
2014-01-02 10:33:14 +0000

    Correct class name

--- htdocs/index.php
+++ htdocs/index.php
@@ -5,4 +5,4 @@ date_default_timezone_set('Europe/London');
 require_once '../config.inc.php';
 require_once '../MailManagerWebService.php';
 
-$ws = new MailManagerWebService($db_config, $smtp_config, $mail_config);
+$ws = new MailManager_WebService($db_config, $smtp_config, $mail_config);
